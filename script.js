ee004f83e363c729c40bcc318a55c13b80acb2d4
vaibhavkambl <142518482+vaibhavkambl@users.noreply.github.com>
2023-10-20 17:04:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
 //your JS code here. If required.
+ document.getElementById('enterBtn').addEventListener('click', function() {
+            document.getElementById('status').innerText = "Entered Metaverse";
+        });
